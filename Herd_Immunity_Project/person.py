dd6dbe9c489da4df1e0e39d42f5e2161e18b8de9
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-27 17:27:47 -0700

    update infection property
    typo fix

--- Herd_Immunity_Project/person.py
+++ Herd_Immunity_Project/person.py
@@ -15,13 +15,13 @@ class Person(object):
     is_alive: Bool. All person objects begin alive (value set to true).  Changed
         to false if person object dies from an infection.
 
-    infection:  None/Virus object.  Set to None for people that are not infected.
+    infection:  None or Virus object.  Set to None for people that are not infected.
         If a person is infected, will instead be set to the virus object the person
         is infected with.
 
     _____Methods_____:
 
-    __init__(self, _id, is_vaccinated, infected=False):
+    __init__(self, _id, is_vaccinated, infection=None):
         - self.alive should be automatically set to true during instantiation.
         - all other attributes for self should be set to their corresponding parameter
             passed during instantiation.
@@ -38,20 +38,20 @@ class Person(object):
             - If random number is smaller, person has died from disease.
                 is_alive is changed to false.
             - If random number is larger, person has survived disease.  Person's
-            is_vaccinated attribute is changed to True, and set self.infected to None.
+            is_vaccinated attribute is changed to True, and set self.infection to None.
     '''
 
-    def __init__(self, _id, is_vaccinated, infected=None):
+    def __init__(self, _id, is_vaccinated, infection=None):
         # TODO:  Finish this method.  Follow the instructions in the class documentation
         # to set the corret values for the following attributes.
         self._id = None
         self.is_vaccinated = None
         self.is_alive = None
-        self.infected = None
+        self.infection = None
 
 
     def did_survive_infection():
         # TODO:  Finish this method. Follow the instructions in the class documentation
         # for resolve_infection.  If person dies, set is_alive to False and return False.
-        # If person lives, set is_vaccinated = True, infected = None, return True.  
+        # If person lives, set is_vaccinated = True, infection = None, return True.  
         pass
