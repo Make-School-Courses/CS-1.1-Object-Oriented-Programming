117267e358ebfdd537b33e070afc414873481f80
Alan Davis <neptunius@gmail.com>
2018-11-28 16:15:53 -0800

    Fix unit test for Virus class

--- Herd_Immunity_Project/virus.py
+++ Herd_Immunity_Project/virus.py
@@ -12,8 +12,8 @@ class Virus(object):
 
 def test_virus_instantiation():
     #TODO: Create your own test that models the virus you are working with
-     '''Check to make sure that the virus instantiator is working.'''
+    '''Check to make sure that the virus instantiator is working.'''
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
-    assert virus.mortality_rate == 0.8
-    assert virus.repro_rate == 0.3
+    assert virus.repro_rate == 0.8
+    assert virus.mortality_rate == 0.3
